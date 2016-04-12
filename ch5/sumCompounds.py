d19e757a9991b48b79a066fcf4046b7447269c7c
megansquire <megansquire@gmail.com>
2016-04-12 14:20:34 -0400

    summarize log by compound score

--- ch5/sumCompounds.py
+++ ch5/sumCompounds.py
@@ -7,10 +7,10 @@ Created on Fri Apr  8 13:26:48 2016
 
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-with open('/Users/megan/Google Drive/research/papers and projects/2016 projects/mastering data mining book/chapters/ch5-sentiment/data/ubuntu2016-04-04/ubuntu.txt', encoding='utf-8') as ubuntu:
+with open('data/ubuntu2016-04-04/ubuntu.txt', encoding='utf-8') as ubuntu:
     ubuntuLines = [line.strip() for line in ubuntu.readlines()]
     
-with open('/Users/megan/Google Drive/research/papers and projects/2016 projects/mastering data mining book/chapters/ch5-sentiment/data/ubuntu2016-04-04/ubuntu-devel.txt', encoding='utf-8') as ubuntuDevel:
+with open('data/ubuntu2016-04-04/ubuntu-devel.txt', encoding='utf-8') as ubuntuDevel:
     ubuntuDevelLines = [line.strip() for line in ubuntuDevel.readlines()]
 
 ubuntu.close()
