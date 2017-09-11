e691ac2c6fb555054cafc3e4c90d856e55f74383
Megan Squire <megansquire@gmail.com>
2017-09-11 14:52:13 -0400

    Update entityAttributeMetrics.py

--- ch3/entityAttributeMetrics.py
+++ ch3/entityAttributeMetrics.py
@@ -7,52 +7,50 @@ Purpose: Finds various string similarity metrics between project URLs stored
 
 """
 import pymysql
-import sys
 from nltk.metrics import *
+import getpass
+from soundex import soundex
 
-password = sys.argv[1]
+# database connection params
+dbhost = 'cs.elon.edu'
+dbschema = 'msquire'
+dbuser = 'msquire'
+dbpasswd = getpass.getpass()
+dbport = 3306
+dbcharset = 'utf8mb4'
 
+# set up queries
+peopleQuery = "SELECT rf.dev_username, rf.dev_realname \
+               FROM rfrg.book_rf_entity_people rf \
+               WHERE rf.project_name =  %s \
+               AND (rf.dev_username IN ( \
+                    SELECT rg.person_name \
+                    FROM rfrg.book_rg_entity_people rg \
+                    WHERE rg.project_name =  %s) \
+                    OR \
+                    rf.dev_realname IN ( \
+                    SELECT rg.person_name \
+                    FROM rfrg.book_rg_entity_people rg \
+                    WHERE rg.project_name = %s))"
 
-def soundex(name, len=4):
-    """
-    soundex module conforming to Knuth's algorithm
-    implementation 2000-12-24 by Gregory Jorgensen
-    public domain
-    available at:
-    http://code.activestate.com/recipes/52213-soundex-algorithm/
-    """
-
-    # digits holds the soundex values for the alphabet
-    digits = '01230120022455012623010202'
-    sndx = ''
-    fc = ''
-
-    # translate alpha chars in name to soundex digits
-    for c in name.upper():
-        if c.isalpha():
-            if not fc: fc = c   # remember first letter
-            d = digits[ord(c)-ord('A')]
-            # duplicate consecutive soundex digits are skipped
-            if not sndx or (d != sndx[-1]):
-                sndx += d
-
-    # replace first digit with first alpha character
-    sndx = fc + sndx[1:]
-
-    # remove all 0s from the soundex code
-    sndx = sndx.replace('0', '')
-
-    # return soundex code padded to len characters
-    return (sndx + (len * '0'))[:len]
-
+updateQuery = "UPDATE book_entity_matches \
+               SET rf_name_soundex    = %s,\
+                   rg_name_soundex    = %s, \
+                   url_levenshtein    = %s, \
+                   name_levenshtein   = %s, \
+                   rf_name_in_rg_name = %s, \
+                   rf_name_in_rg_url  = %s, \
+                   rf_dev_in_rg_dev   = %s \
+               WHERE rf_project_name = %s \
+               AND rg_project_name = %s"
 
 # Open local database connection
-db = pymysql.connect(host='cs.elon.edu',
-                     db='rfrg',
-                     user='msquire',
-                     passwd=password,
-                     port=3306,
-                     charset='utf8mb4',
+db = pymysql.connect(host=dbhost,
+                     db=dbschema,
+                     user=dbuser,
+                     passwd=dbpasswd,
+                     port=dbport,
+                     charset=dbcharset,
                      autocommit=True)
 cursor = db.cursor()
 
@@ -60,15 +58,15 @@ cursor = db.cursor()
 cursor.execute("INSERT INTO book_entity_matches (rf_project_name, \
                                                 rg_project_name) \
                 SELECT rf.project_name, rg.project_name \
-                FROM book_rf_entities rf \
-                INNER JOIN book_rg_entities rg ON rf.url = rg.url")
+                FROM rfrg.book_rf_entities rf \
+                INNER JOIN rfrg.book_rg_entities rg ON rf.url = rg.url")
 
 # get additional projects that have matching project names
 cursor.execute("INSERT INTO book_entity_matches(rf_project_name, \
                                                 rg_project_name) \
                 SELECT rf.project_name, rg.project_name \
-                FROM book_rf_entities rf \
-                INNER JOIN book_rg_entities rg \
+                FROM rfrg.book_rf_entities rf \
+                INNER JOIN rfrg.book_rg_entities rg \
                 ON rf.project_name = rg.project_name \
                 WHERE rf.project_name NOT IN ( \
                     SELECT bem.rf_project_name \
@@ -80,10 +78,10 @@ cursor.execute("SELECT bem.rf_project_name, \
                        bem.rg_project_name, \
                        rfe.url, \
                        rge.url \
-                FROM book_entity_matches bem \
-                INNER JOIN book_rg_entities rge \
+                FROM rfrg.book_entity_matches bem \
+                INNER JOIN rfrg.book_rg_entities rge \
                   ON bem.rg_project_name = rge.project_name \
-                INNER JOIN book_rf_entities rfe \
+                INNER JOIN rfrg.book_rf_entities rfe \
                   ON bem.rf_project_name = rfe.project_name \
                 ORDER BY bem.rf_project_name")
 projectPairs = cursor.fetchall()
@@ -119,18 +117,7 @@ for(projectPair) in projectPairs:
         rf_in_rgurl = 0
 
     # is any dev on the RF candidate in the dev list for the RG candidate?
-    cursor.execute("SELECT rf.dev_username, rf.dev_realname \
-                    FROM book_rf_entity_people rf \
-                    WHERE rf.project_name =  %s \
-                    AND (rf.dev_username IN ( \
-                        SELECT rg.person_name \
-                        FROM book_rg_entity_people rg \
-                        WHERE rg.project_name =  %s) \
-                        OR \
-                        rf.dev_realname IN ( \
-                        SELECT rg.person_name \
-                        FROM book_rg_entity_people rg \
-                        WHERE rg.project_name = %s))",
+    cursor.execute(peopleQuery,
                    (RFname, RGname, RGname))
     result = cursor.fetchone()
     if result is not None:
@@ -138,16 +125,7 @@ for(projectPair) in projectPairs:
     else:
         rfdev_in_rgdev = 0
 
-    cursor.execute("UPDATE book_entity_matches \
-                        SET rf_name_soundex    = %s,\
-                            rg_name_soundex    = %s, \
-                            url_levenshtein    = %s, \
-                            name_levenshtein   = %s, \
-                            rf_name_in_rg_name = %s, \
-                            rf_name_in_rg_url  = %s, \
-                            rf_dev_in_rg_dev   = %s \
-                        WHERE rf_project_name = %s \
-                        AND rg_project_name = %s",
+    cursor.execute(updateQuery,
                    (soundexRFname,
                     soundexRGname,
                     levURLs,
