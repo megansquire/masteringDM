dd6e825ccf406ba24921503b74e5a55d7a5b6389
Megan Squire <megansquire@gmail.com>
2017-09-19 15:52:09 -0400

    Update basicNetworkMetrics4.py

--- ch4/basicNetworkMetrics4.py
+++ ch4/basicNetworkMetrics4.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Feb 28 10:53:27 2016
+Updated on Tues Sep 19 15:51:00 2017
 
 @author: megan squire
 """
@@ -14,14 +15,16 @@ graphsSorted = sorted(graphs, key=len, reverse=True)
 i = 0
 for graph in graphsSorted[0:5]:
     i += 1
-    print(nx.number_of_nodes(graph))
+    print('Number of nodes in graph', i, ':', nx.number_of_nodes(graph))
     graphDegree = nx.degree(graph)
     
     cliques = list(nx.find_cliques(graph))
-    print('cliques for graph' + str(i))
+    print('Cliques for graph ', i)
     print(cliques) 
 
     ev = nx.eigenvector_centrality_numpy(graph) 
     evSorted = sorted(ev.items(), key=operator.itemgetter(1), reverse=True)
     for key,val in evSorted:
         print(key,str(round(val,2)))
+    print("===")
+    
