02f4cd71c03d5b9c1e2ba99e0a860e47ad8e6f17
Megan Squire <megansquire@gmail.com>
2017-11-04 13:33:07 -0400

    Update findNEinFile.py

--- ch6/findNEinFile.py
+++ ch6/findNEinFile.py
@@ -27,13 +27,27 @@ try:
         tagged = nltk.pos_tag(tokenized)
         chunked = nltk.ne_chunk(tagged)
       
+        # this was the original code in the book:
+        '''
         for tree in chunked:
             if hasattr(tree, 'label'):
                 ne = ' '.join(c[0] for c in tree.leaves())
                 en[ne] = [tree.label(), ' '.join(c[1] for c in tree.leaves())]
-            
+        '''
+        # here is another way to write it that might be easier for new programmers"
+        for tree in chunked:
+            if hasattr(tree, 'label'):
+                ne = ''
+                for c in tree.leaves():
+                    ne += c[0] + ' '
+                ne = ne.rstrip()
+                en[ne] = tree.label()
+    for key in en.keys():
+        print(key, ':', en[key])
 except Exception as e:
     print(str(e))
     
+'''
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(en)
+'''
